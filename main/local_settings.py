57de41b0e4b72d2176ed00bd8d0ce56066c407e2
ManFromRzeszotary <roman.tonia@gmail.com>
2022-11-21 17:59:16 +0100

    initial commit

---
+++ main/local_settings.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'charity-donation',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+    }
+}
---
+++ stuffshare/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StuffshareConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stuffshare'
